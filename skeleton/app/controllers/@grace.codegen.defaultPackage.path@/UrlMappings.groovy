b0662848abcabdd3b68360b0e4bbd01cbecf7412
Michael Yan <rain@rainboyan.com>
2026-03-27 14:30:17 +0800

    Code formatting for skeleton
    Closes gh-61

--- skeleton/app/controllers/@grace.codegen.defaultPackage.path@/UrlMappings.groovy
+++ skeleton/app/controllers/@grace.codegen.defaultPackage.path@/UrlMappings.groovy
@@ -3,15 +3,16 @@ package @grace.codegen.defaultPackage@
 class UrlMappings {
 
     static mappings = {
-        delete "/$controller/$id(.$format)?"(action:"delete")
-        get "/$controller(.$format)?"(action:"index")
-        get "/$controller/$id(.$format)?"(action:"show")
-        post "/$controller(.$format)?"(action:"save")
-        put "/$controller/$id(.$format)?"(action:"update")
-        patch "/$controller/$id(.$format)?"(action:"patch")
+        delete "/$controller/$id(.$format)?"(action: 'delete')
+        get "/$controller(.$format)?"(action: 'index')
+        get "/$controller/$id(.$format)?"(action: 'show')
+        post "/$controller(.$format)?"(action: 'save')
+        put "/$controller/$id(.$format)?"(action: 'update')
+        patch "/$controller/$id(.$format)?"(action: 'patch')
 
-        "/"(controller: 'application', action:'index')
-        "500"(view: '/error')
-        "404"(view: '/notFound')
+        '/'(controller: 'application', action: 'index')
+        '500'(view: '/error')
+        '404'(view: '/notFound')
     }
+
 }
